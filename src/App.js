406ff7c5d9bbf8e4d28d56e22487938e38b97886
clydeyuan07 <panes.cl.bsinfotech@gmail.com>
2025-05-16 12:13:44 +0800

    First Push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">FixMyHome - React Frontend</h1>
+    </div>
+  );
+}
+
+export default App;
+
